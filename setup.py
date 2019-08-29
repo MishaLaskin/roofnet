9e38c39a292c8416c970b569f0d94fd9c0020848
MishaLaskin <laskin.misha@gmail.com>
2019-08-29 09:48:25 -0700

    data preprocessing and loading

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='roofnet',
+      version='0.1',
+      description='Code for RoofNet',
+      url='http://github.com/MishaLaskin/roofnet',
+      author='ML',
+      author_email='laskin.misha@gmail.com',
+      license='MIT',
+      packages=['roofnet'],
+      zip_safe=False)
